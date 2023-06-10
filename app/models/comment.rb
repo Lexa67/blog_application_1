85336da49c6e4664456cbf8488f81c1545604c9c
Lexa67 <3aj4ik504@gmail.com>
2023-06-10 16:59:07 +0300

    10.06.2023_16:59

---
+++ app/models/comment.rb
@@ -0,0 +1,29 @@
+class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :user
+  has_rich_text :body
+
+  validate :validate_comment_count, on: :create
+
+  after_create_commit :notify_recipient
+  before_destroy :cleanup_notifications
+  has_noticed_notifications model_name: 'Notification'
+
+  def validate_comment_count
+    max_comments_per_user = 3 # Максимальное количество комментариев для каждого пользователя
+    if user.comments.where(post_id: post.id).count >= max_comments_per_user
+      errors.add(:base, "Достигнуто максимальное количество комментариев для данного пользователя")
+    end
+  end
+
+  private
+
+  def notify_recipient
+    CommentNotification.with(comment: self, post: post).deliver_later(post.user)
+  end
+
+  def cleanup_notifications
+    notifications_as_comment.destroy_all
+  end
+
+end
